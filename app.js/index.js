bf692aef6e07f178726ef25a9bbd16cd3c0b0715
Nath <nathalia2602rodrigues@gmail.com>
2026-07-01 20:57:45 -0300

    Add files via upload

---
+++ app.js/index.js
@@ -0,0 +1,9 @@
+let nome = process.argv[2];
+let classe = process.argv[3];
+let nivel = process.argv[4];
+
+console.log("===== Cadastro do personagem =====");
+
+console.log("Nome:", nome);
+console.log("Classe:", classe);
+console.log("nivel:", nivel);
