4f20bb30fd103435e1d09c980dffce0b446d3344
NATHÁLIA LETICIA RODRIGUES <nathalia_rodrigue150@estudante.sesisenai.org.br>
2026-07-01 21:46:48 -0300

    Exercicio 3 feito

---
+++ app.js/Nivel 1/EX 3/ficha.js
@@ -0,0 +1,12 @@
+let nome = "Argila";
+let classe = "Mago";
+let nivel = 40;
+let vida = 25;
+let mana = 16;
+
+console.log("Nome:", nome);
+console.log("Classe:", classe);
+console.log("Nível:", nivel);
+console.log("Vida;", vida);
+console.log("Mana:", mana);
+
