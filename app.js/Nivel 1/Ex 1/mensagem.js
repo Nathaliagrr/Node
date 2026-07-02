cc08f582aefba7cf47a343f507aaf022b0986be6
NATHÁLIA LETICIA RODRIGUES <nathalia_rodrigue150@estudante.sesisenai.org.br>
2026-07-01 21:30:58 -0300

    Resolução dos exercicios 1 e 2

---
+++ app.js/Nivel 1/Ex 1/mensagem.js
@@ -0,0 +1,7 @@
+let nome = process.argv[2];
+let curso = process.argv[3];
+let motivacional = process.argv[4];
+
+console.log("Nome:", nome);
+console.log("Curso:", curso);
+console.log("Motivacional:", motivacional);
---
+++ app.js/Nivel 1/Ex 2/calculadora.js
@@ -0,0 +1,15 @@
+let numero1 = 7;
+let numero2 = 10;
+
+console.log("Numero 1:", numero1);
+console.log("Numero 2:", numero2);
+
+let soma = numero1 + numero2;
+let subtracao = numero2 - numero1;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+console.log("Soma=", soma);
+console.log("subtracao=", subtracao);
+console.log("multiplicação=", multiplicacao);
+console.log("Divisão=", divisao);
